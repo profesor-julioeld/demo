cad0eaf4834dc97d83220664c380a6c1553f7be1
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2022-05-19 14:04:44 +0200

    Init inicial

---
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.s2daw.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value="prueba")
+    public String prueba(){
+        return "prueba";
+    }
+}
