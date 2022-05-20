e3149e0f41e476f1e518541743a581268325b2c4
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2022-05-20 12:17:03 +0200

    Hasta minuto 01:17:00 del curso

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -3,11 +3,13 @@ package com.s2daw.demo.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
     @RequestMapping(value="prueba")
-    public String prueba(){
-        return "prueba";
+    public List<String> prueba(){
+        return List.of("prueba","prueba2","prueba3");
     }
 }
