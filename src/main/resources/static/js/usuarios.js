20ce30e56ad8f39f4791f66a2cecd0bde129742e
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2022-05-23 12:53:38 +0200

    Hasta minuto 01:55:25 del curso ( backend devuelve lista de usuarios (sin acceder a BD), frontend muestra la lista en una tabla)

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 async function cargaUsuarios(){
-  const request = await fetch('usuario/234', {
+  const request = await fetch('usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -13,5 +13,16 @@ async function cargaUsuarios(){
     }
   });
   const usuarios = await request.json();
+  let listadoHTML='';
+  for (let usuario of usuarios){
+    let usuarioHTML= '<tr><td>'+usuario.id+
+                     '<td>'+usuario.nombre+
+                     '</td><td>'+usuario.email+
+                     '</td><td>'+usuario.telefono+
+                     '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td><tr>';
+    listadoHTML+=usuarioHTML;
+  }
   console.log(usuarios);
+
+  document.querySelector('#usuarios tbody').outerHTML=listadoHTML;
 }
