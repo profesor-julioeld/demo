5a1dd3bf787ecf0175807caf7c2c1a23d3c7bdcb
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2022-05-23 09:23:40 +0200

    Hasta minuto 01:41:33 del curso (Modifica usuarios.js para que consuma backend mediante fetch, backend devuelve datos sin acceder a BD)

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,5 +1,17 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  alert("Esto es una prueba");
+  cargaUsuarios();
   $('#usuarios').DataTable();
 });
+
+async function cargaUsuarios(){
+  const request = await fetch('usuario/234', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+  const usuarios = await request.json();
+  console.log(usuarios);
+}
