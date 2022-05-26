41aaf140649c701a7ad87d31bdd40aa2d510b294
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2022-05-26 19:34:33 +0200

    Hasta minuto 02:25:40 del curso ( backend devuelve lista de usuarios accediendo a BD,   se crea conexión a BD MariaDB,   se define elementos JPA)

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.dao.UsuarioDao;
 import com.s2daw.demo.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,8 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioDao usuarioDao;
     @RequestMapping(value="usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id){
         Usuario usuario=new Usuario();
@@ -23,31 +27,6 @@ public class UsuarioController {
     }
     @RequestMapping(value="usuarios")
     public List<Usuario> getUsuarios(){
-        List<Usuario> usuarios=new ArrayList<>();
-        Usuario usuario=new Usuario();
-        usuario.setId(1L);
-        usuario.setNombre("Lucas");
-        usuario.setApellidos("Moy");
-        usuario.setEmail("lucasmoy@hotmail.com");
-        usuario.setTelefono("234234234");
-        usuarios.add(usuario);
-
-        usuario=new Usuario();
-        usuario.setId(2L);
-        usuario.setNombre("Lucas2");
-        usuario.setApellidos("Moy2");
-        usuario.setEmail("lucasmoy2@hotmail.com");
-        usuario.setTelefono("12");
-        usuarios.add(usuario);
-
-        usuario=new Usuario();
-        usuario.setId(3L);
-        usuario.setNombre("Lucas3");
-        usuario.setApellidos("Moy3");
-        usuario.setEmail("lucasmoy3@hotmail.com");
-        usuario.setTelefono("23");
-        usuarios.add(usuario);
-
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 }
